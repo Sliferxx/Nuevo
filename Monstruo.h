b23bf0b00c2a6b8a60ff4d92d6c165ac13c07a80
Sliferxx <kaioken20_70@live.com>
2015-06-04 05:37:03 -1000

    Create Monstruo.h

---
+++ Monstruo.h
@@ -0,0 +1,17 @@
+
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+typedef struct TipoMonstruo{
+  int vida;
+  int defensa;
+  int ataque;
+  int ptos;
+}Monstruo;
